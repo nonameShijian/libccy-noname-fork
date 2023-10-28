cbbc8d27294d310fa0ee7cc5065574fd417af438
shijian <2954700422@qq.com>
2023-10-29 02:00:08 +0800

    拆分game.js，目前缺点:lib和game无类型提示，file协议浏览器无法使用

---
+++ game/modules/gnc.js
@@ -0,0 +1,40 @@
+require('./gnc.js');
+const GeneratorFunction = (function* () { }).constructor;
+const gnc = Object.assign(module.exports, {
+	of: fn => gnc.is.generatorFunc(fn) ? function genCoroutine() {
+		let gen = fn.apply(this, arguments);
+		gen.status = "next";
+		gen.state = undefined;
+		const callback = (resolve, reject) => {
+			let result,
+				nexts = resolve,
+				throws = reject;
+			try {
+				result = gen[gen.status](gen.state);
+			} catch (error) {
+				reject(error);
+				return;
+			}
+			if (!result.done) {
+				nexts = (item) => {
+					gen.state = item;
+					gen.status = "next";
+					callback(resolve, reject);
+				}
+				throws = (err) => {
+					gen.state = err;
+					gen.status = "throw";
+					callback(resolve, reject);
+				}
+			}
+			result = result.value;
+			Promise.resolve(result).then(nexts, throws);
+		}
+		return new Promise(callback);
+	} : (() => { throw new TypeError("gnc.of needs a GeneratorFunction.") })(),
+	is: {
+		coroutine: item => typeof item == "function" && item.name == "genCoroutine",
+		generatorFunc: item => item instanceof GeneratorFunction,
+		generator: item => (typeof item == "object") && ("constructor" in item) && item.constructor && ("constructor" in item.constructor) && item.constructor.constructor === GeneratorFunction
+	}
+});
